c36a0f025615b70e7c9376e2dbb60ed44c56d8ac
suman <sumankshetri23@gmail.com>
2020-08-08 21:37:29 -0400

    "username generator + auth controller"

--- src/main/java/com/customer/controller/AuthController.java
+++ src/main/java/com/customer/controller/AuthController.java
@@ -1,5 +1,31 @@
 package com.customer.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class AuthController {
+	
+	@GetMapping({"/","/auth"})
+	public String authform() {
+		return "index";
+	}
+	
+	@PostMapping("/auth")
+	public String authUser(@RequestParam String usernameOrEmail, @RequestParam String password) {
+		//CustomerDTO customerDTO = customerService.authUser(usernameOrEmail,password);
+		
+		/*
+		 * if (true) return "dashboard"; else return "error";
+		 */
+		return "success";
+			
+	}
+	
+	
+	
+	
 
 }
